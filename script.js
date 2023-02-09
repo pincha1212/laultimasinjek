1720788a896ab2982488c38a76dfe113d3176d3a
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2023-02-08 23:38:13 -0300

    animaciones boton1

--- script.js
+++ script.js
@@ -38,16 +38,62 @@ square.style.display = "block";
 gsap.to(square, {duration: 1, x: '134.5%', y: '0%', onComplete: function() {
 blurElements.classList.add("blur");
 closeBtn.style.display = "block";
-gsap.fromTo(previousBtn, {opacity: 0}, {duration: 1, delay: 0.5, opacity: 1});
-gsap.fromTo(nextBtn, {opacity: 0}, {duration: 1, delay: 0.5, opacity: 1});
-gsap.fromTo(descripcion1, {opacity: 0, x: 100}, {duration: 1, delay: 0.5, opacity: 1, x:0});
-gsap.fromTo(titulo1, {opacity: 0, x: -100}, {duration: 1, delay: 1, opacity: 1, x: 0});
-gsap.fromTo(subtitulo1, {opacity: 0, x: 100}, {duration: 1, delay: 1.5, opacity: 1, x: 0});
-gsap.fromTo(socialIcon, {opacity: 0, x: -100}, {duration: 1, delay: 2, opacity: 1, x: 0});
-
+  
 }});
 });
 
+
+var descripcion1 = document.querySelector(".descripcion1");
+
+descripcion1.addEventListener("mouseenter", function() {
+  gsap.to(descripcion1, {duration: 1, scale: 1.2, backgroundColor: "black", borderRadius: "20px"});
+});
+
+descripcion1.addEventListener("mouseleave", function() {
+  gsap.to(descripcion1, {duration: 1, scale: 1, backgroundColor: "transparent", borderRadius: "0"});
+});
+
+
+
+var titulo1 = document.querySelector(".titulo1");
+
+openBtn.addEventListener("click", function() {
+
+  gsap.from(titulo1, {duration: 1, opacity: 0, y: -25, ease: "bounce", delay: 2});
+});
+
+var subtitulo1 = document.querySelectorAll(".subtitulo1");
+
+openBtn.addEventListener("click", function() {
+  gsap.from(subtitulo1, {duration: 1, opacity: 0, y: -25, ease: "bounce", delay: 2.5}); 
+});
+
+var info = document.querySelectorAll(".info");
+
+openBtn.addEventListener("click", function(){
+
+  gsap.from(info, {duration: 1, opacity: 0, y: -25, ease: "bounce", delay: 3.5});
+});
+
+var playButton = document.querySelector(".music-button");
+var audio = document.querySelector("#audio");
+
+playButton.addEventListener("click", function() {
+  playButton.style.display = "none";
+  audio.style.display = "block";
+  closeButton.style.display = "block";
+  gsap.from(audio, {duration: 2, opacity: 1, y: 0, ease: "bounce",delay: 2});
+});
+
+var closeButton = document.querySelector("#close-button");
+var audio = document.querySelector("#audio");
+
+closeButton.addEventListener("click", function() {
+  playButton.style.display = "block";
+  audio.style.display = "none";
+  closeButton.style.display = "none";
+  gsap.to(audio, {duration: 1, opacity: 0, y: 0, ease: "bounce", delay:2});
+});
 
   
   closeBtn.addEventListener("click", function() {
